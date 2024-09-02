7f76338cf912a1251c655ae49cf5068bdb040d2b
hmy <hmy1218@qq.com>
2024-09-02 17:46:47 +0800

    基础篇-05.功能详解-数据字典

---
+++ src/views/statistics/customer.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts' name="Customer">
+
+</script>
+
+<template>
+    <div class="customer" >
+        <h2>customer</h2>
+    </div>
+</template>
+
+<style lang='less' scoped>
+.customer {
+  color: red;
+}
+</style>
